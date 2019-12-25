18d657636f58c537a07d1df433bf34921d0a6167
bbakewaikhom <bbakewai@gmail.com>
2019-12-25 22:14:16 +0530

    added add_event function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,31 @@
 import * as functions from 'firebase-functions';
+import admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+let db = admin.firestore();
+let timestamp = admin.firestore.Timestamp
 
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+export const add_event = functions.https.onRequest(async (request, response) => {
+    console.log(request.rawBody)
+
+    const data = request.body
+
+    await db.collection('Event').add({
+        title: data.title,
+        event_leader: data.event_leader,
+        assign_by: data.assign_by,
+        created: timestamp.fromDate(new Date()),
+        date: timestamp.fromDate(new Date(data.date)),
+        description: data.description,
+        selected_team: getArrayString(data.selected_team),
+        organizers: getArrayString(data.organizers),
+    }).then((ref: any) => {
+        console.log('Added document with ID: ', ref.id);
+    });
+
+    response.status(200).send("200");
 });
+
+function getArrayString(str: string) {
+    return str.substring(1, (str.length - 1)).split(",")
+}
